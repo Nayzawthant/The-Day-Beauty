bd0090bf5709ab0eb1b8cbbcf8f529f9b8d6853a
Nayzawthant <108381970+Nayzawthant@users.noreply.github.com>
2022-07-28 20:24:34 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+let time = document.getElementById("current-time");
+
+setInterval(() =>{
+    let d = new Date();
+    time.innerHTML = d.toLocaleTimeString();
+},1000);
+ */
+
+/*
+    Icon.onclick = function() {
+        if(Navbar.style.display =="block") {
+            Navbar.style.display ="none"
+            this.style.display ="block"
+        }else{
+            Navbar.style.display = "block"
+        }
+    }
+*/
